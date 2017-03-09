c86b81bfae5f6fee5a5fab5dfdd26aceed032e69
Rayjk123 <Branden@brandens-macbook-air.local>
2017-03-09 14:54:49 -0500

    Add Files

--- Comfy-Notes/ViewController.swift
+++ Comfy-Notes/ViewController.swift
@@ -8,18 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class NotesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var tableview : UITableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTableView()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func setupTableView(){
+        tableview.frame = UIScreen.main.bounds
+        tableview.delegate = self
+        tableview.dataSource = self
+        tableview.register(UITableViewCell.self, forCellReuseIdentifier: "noteCell")
+        tableview.tableFooterView = UIView()
+        tableview.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(tableview)
+        
+        tableview.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableview.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableview.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableview.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        let label = UILabel(frame: CGRect(x: 20, y: 10, width: UIScreen.main.bounds.width - 60, height: 30))
+        label.centerYAnchor.constraint(equalTo: cell.centerYAnchor)
+        label.text = String(indexPath.row)
+        cell.addSubview(label)
+        return cell;
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
 
 }
 
